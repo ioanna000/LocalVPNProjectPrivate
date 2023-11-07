23e33b6f0e49ed7da9878498d755b1e59d1f8942
ioanna000 <ioannakig2013@gmail.com>
2023-11-07 20:08:23 +0200

    Initial commit

---
+++ app/src/main/java/xyz/hexene/localvpn/LocalVPNService.java
@@ -0,0 +1,302 @@
+/*
+** Copyright 2015, Mohamed Naufal
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+package xyz.hexene.localvpn;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.Build;
+import android.os.ParcelFileDescriptor;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.Closeable;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LocalVPNService extends VpnService
+{
+    private static final String TAG = LocalVPNService.class.getSimpleName();
+
+    //discuss Is this address supposed to change if I want to make it work or it's ok?
+    private static final String VPN_ADDRESS = "10.0.0.2"; // Only IPv4 support for now
+    private static final String VPN_ROUTE = "0.0.0.0"; // Intercept everything
+
+    public static final String BROADCAST_VPN_STATE = "xyz.hexene.localvpn.VPN_STATE";
+
+    private static boolean isRunning = false;
+
+    private ParcelFileDescriptor vpnInterface = null;
+
+    private PendingIntent pendingIntent;
+
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+    private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+    private ExecutorService executorService;
+
+    private Selector udpSelector;
+    private Selector tcpSelector;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        isRunning = true;
+        setupVPN();
+        try
+        {
+            udpSelector = Selector.open();
+            tcpSelector = Selector.open();
+            deviceToNetworkUDPQueue = new ConcurrentLinkedQueue<>();
+            deviceToNetworkTCPQueue = new ConcurrentLinkedQueue<>();
+            networkToDeviceQueue = new ConcurrentLinkedQueue<>();
+
+            executorService = Executors.newFixedThreadPool(5);
+            executorService.submit(new UDPInput(networkToDeviceQueue, udpSelector));
+            executorService.submit(new UDPOutput(deviceToNetworkUDPQueue, udpSelector, this));
+            executorService.submit(new TCPInput(networkToDeviceQueue, tcpSelector));
+            executorService.submit(new TCPOutput(deviceToNetworkTCPQueue, networkToDeviceQueue, tcpSelector, this));
+            executorService.submit(new VPNRunnable(vpnInterface.getFileDescriptor(),
+                    deviceToNetworkUDPQueue, deviceToNetworkTCPQueue, networkToDeviceQueue));
+            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(BROADCAST_VPN_STATE).putExtra("running", true));
+            Log.i(TAG, "Started");
+        }
+        catch (IOException e)
+        {
+            // TODO: Here and elsewhere, we should explicitly notify the user of any errors
+            // and suggest that they stop the service, since we can't do it ourselves
+            Log.e(TAG, "Error starting service", e);
+            cleanup();
+        }
+    }
+
+    private void setupVPN()
+    {
+        if (vpnInterface == null)
+        {
+            Builder builder = new Builder();
+
+            //VPN_ADDRESS = "10.0.0.2" still don't get why it's this specifically
+            builder.addAddress(VPN_ADDRESS, 32);
+
+            //VPN_ROUTE = "0.0.0.0" which means intercept everything
+            builder.addRoute(VPN_ROUTE, 0);
+
+            //DISCUSS-- If I want to get only the private traffic I suppose that's a way to do it? --//
+            //DISCUSS-- Would that be incoming and outgoing? Just outgoing? What happens with NAT in the middle? --//
+
+            //DISCUSS -- nsure that only the local network traffic passes through your VPN service, while other
+            // traffic continues to use the device's normal network connection.
+
+            //builder.addRoute(10.0.0.0, 8);
+            //builder.addRoute(172.16.0.0, 12);
+            //builder.addRoute(192.168.0.0, 16);
+
+            vpnInterface = builder.setSession(getString(R.string.app_name)).setConfigureIntent(pendingIntent).establish();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        return START_STICKY;
+    }
+
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        isRunning = false;
+        executorService.shutdownNow();
+        cleanup();
+        Log.i(TAG, "Stopped");
+    }
+
+    private void cleanup()
+    {
+        deviceToNetworkTCPQueue = null;
+        deviceToNetworkUDPQueue = null;
+        networkToDeviceQueue = null;
+
+        //CHANGES: MADE CHANGE HERE TO MATCH THE CURRENT MIN SDK FROM 14 to 19.
+        //CHANGES: Don't believe it's a problem since most phones are on above 21 atm
+        ByteBufferPool.clear();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            closeResources(udpSelector, tcpSelector, vpnInterface);
+        }
+    }
+
+    // TODO: Move this to a "utils" class for reuse
+    private static void closeResources(Closeable... resources)
+    {
+        for (Closeable resource : resources)
+        {
+            try
+            {
+                resource.close();
+            }
+            catch (IOException e)
+            {
+                // Ignore
+            }
+        }
+    }
+
+    private static class VPNRunnable implements Runnable
+    {
+        private static final String TAG = VPNRunnable.class.getSimpleName();
+
+
+        //changes Apparently an interface is a kind of file you need to read with a file descriptor from what i got
+        private FileDescriptor vpnFileDescriptor;
+
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+        private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+
+        public VPNRunnable(FileDescriptor vpnFileDescriptor,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue,
+                           ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue)
+        {
+            this.vpnFileDescriptor = vpnFileDescriptor;
+            this.deviceToNetworkUDPQueue = deviceToNetworkUDPQueue;
+            this.deviceToNetworkTCPQueue = deviceToNetworkTCPQueue;
+            this.networkToDeviceQueue = networkToDeviceQueue;
+        }
+
+        @Override
+        public void run()
+        {
+            Log.i(TAG, "Started");
+
+            //Changes FileChannel is a channel to read from a file and write to a file
+
+
+            //discuss: Packets coming from the network towards the device's app
+            //The bytestream getting into the vpnchannel
+            FileChannel vpnInput = new FileInputStream(vpnFileDescriptor).getChannel();
+
+
+            //FileChannel vpnOutput for writing data to the VPN service's discuss: file descriptor(still not fully aware of what it is here).
+            // Discuss: Packets from Android app to the VPN service??
+            FileChannel vpnOutput = new FileOutputStream(vpnFileDescriptor).getChannel();
+
+            try
+            {
+                ByteBuffer bufferToNetwork = null;
+
+                //changes dataSent is for checking the flow from the VPN tunnel to the app of the device
+                boolean dataSent = true;
+
+                //changes dataReceived is for checking the flow from the app of the device to the VPN tunnel
+                boolean dataReceived;
+
+                //changes While loop for packet processing
+                while (!Thread.interrupted())
+                {
+                    if (dataSent)
+                        bufferToNetwork = ByteBufferPool.acquire();
+                    else
+                        bufferToNetwork.clear();
+
+                    // TODO: Block when not connected
+                    // discuss: Would blocking queue be a good fit?
+                    int readBytes = vpnInput.read(bufferToNetwork);
+                    if (readBytes > 0)
+                    {
+                        dataSent = true;
+                        bufferToNetwork.flip();
+                        Packet packet = new Packet(bufferToNetwork);
+
+                        if (packet.isUDP())
+                        {
+                            Log.w(TAG, "\n\nIPV4 UDP PACKETTTTTTTTTTTTTTTT:   " + packet.ip4Header.destinationAddress.getHostAddress() + "\n\n");
+                            Log.w(TAG, "This device IP address" + InetAddress.getLocalHost().getHostAddress());
+                            deviceToNetworkUDPQueue.offer(packet);
+
+                        }
+                        else if (packet.isTCP())
+                        {
+                            Log.w(TAG, "\n\nIPV4 TCP PACKETTTTTTTTTTTTTTTT:   " + packet.ip4Header.destinationAddress.getHostAddress() + "\n\n");
+                            Log.w(TAG, "This device IP address" + InetAddress.getLocalHost());
+
+                            deviceToNetworkTCPQueue.offer(packet);
+                        }
+                        else
+                        {
+                            Log.w(TAG, "Unknown packet type");
+                            Log.w(TAG, packet.ip4Header.toString());
+                            dataSent = false;
+                        }
+                    }
+                    else
+                    {
+                        dataSent = false;
+                    }
+
+                    ByteBuffer bufferFromNetwork = networkToDeviceQueue.poll();
+                    if (bufferFromNetwork != null)
+                    {
+                        bufferFromNetwork.flip();
+                        while (bufferFromNetwork.hasRemaining())
+                            vpnOutput.write(bufferFromNetwork);
+                        dataReceived = true;
+
+                        ByteBufferPool.release(bufferFromNetwork);
+                    }
+                    else
+                    {
+                        dataReceived = false;
+                    }
+
+                    // TODO: Sleep-looping is not very battery-friendly, consider blocking instead
+                    // Confirm if throughput with ConcurrentQueue is really higher compared to BlockingQueue
+                    if (!dataSent && !dataReceived)
+                        Thread.sleep(10);
+                }
+            }
+            catch (InterruptedException e)
+            {
+                Log.i(TAG, "Stopping");
+            }
+            catch (IOException e)
+            {
+                Log.w(TAG, e.toString(), e);
+            }
+            finally
+            {
+                closeResources(vpnInput, vpnOutput);
+            }
+        }
+    }
+}
